a4182130f379d594927ef3bb8824d3d12586bea5
Manoj <Manoj>
2020-10-01 17:44:52 -0400

    First commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Manoj on 2020-10-01.
+//  Copyright © 2020 Manoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
