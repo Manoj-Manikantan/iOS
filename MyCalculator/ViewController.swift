478614d5943c79385ce6b1e65601f0e079a971f3
Manoj Manikantan <manojmkt127@gmail.com>
2020-10-02 17:58:03 -0400

    chore-comments added

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -1,7 +1,14 @@
-//
-//  ViewController.swift
-//  MyCalculator
-//
+//  Project Name : MyCalculator
+//  File Name : ViewController.swift
+//  Author's Name : Manoj Manikantan Muralidharan
+//  Student ID : 301067347
+//  Date : 2nd Oct 2020
+//  Description : Created UI for MyCalculator based on the wireframe I created
+//  Version : V1.0
+
+
+// MyCalculator
+//	
 //  Created by Manoj on 2020-10-01.
 //  Copyright © 2020 Manoj. All rights reserved.
 //
