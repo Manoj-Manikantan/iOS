1527a15486e1188ac2bce321c1959b594dd306e6
Manoj <Manoj>
2020-10-07 17:04:11 -0400

    Assignment2 created

---
+++ Assignment2/MyCalculator/ViewController.swift
@@ -0,0 +1,21 @@
+//  Project Name : MyCalculator - Basic calculator functionalities
+//  File Name : ViewController.swift
+//  Author's Name : Manoj Manikantan Muralidharan
+//  Assignment 2
+//  Student ID : 301067347
+//  Date : 7th Oct 2020
+//  Description : Created UI for MyCalculator based on the wireframe I created
+//  Version : V1.0
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
