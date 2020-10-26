1da167189160f6b58af495f0824938a94981018a
Manoj <Manoj>
2020-10-25 22:05:33 -0400

    feat: stack view visible for landscape added

--- Assignment3/MyCalculator/MainViewController.swift
+++ Assignment3/MyCalculator/MainViewController.swift
@@ -29,149 +29,167 @@ class MainViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    @IBOutlet weak var set3OperationsStackView: UIStackView!
+    
+    /* Makes the stack view visible in Landscape mode */
+    @objc func rotated()
+    {
+        if UIDevice.current.orientation.isLandscape {
+            set3OperationsStackView.isHidden = false
+        } else {
+            set3OperationsStackView.isHidden = true
+        }
     }
     
     @IBAction func OnButton_Press(_ sender: UIButton)
     {
         switch sender.titleLabel!.text!
         {
-            case "√":
-                if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
-                {
-                    currentNumberValue = Double(ResultLabel.text!)!
-                    finalResult = String(currentNumberValue.squareRoot())
-                    displayResult(outputVal: finalResult)
-                }
-            case "Rand":
-                randomDouble = Double.random(in: 0.21828...9.14159)
-                ResultLabel.text! = String(randomDouble)
-                if (firstOperatorValue == 0)
-                {
-                    firstOperatorValue = currentNumberValue
-                }
+        case "√":
+            if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
+            {
                 currentNumberValue = Double(ResultLabel.text!)!
-            case "𝝅":
-                ResultLabel.text! = String(Double.pi)
-                if (firstOperatorValue == 0)
-                {
-                    firstOperatorValue = currentNumberValue
-                }
+                finalResult = String(currentNumberValue.squareRoot())
+                displayResult(outputVal: finalResult)
+            }
+        case "Rand":
+            randomDouble = Double.random(in: 0.21828...9.14159)
+            ResultLabel.text! = String(randomDouble)
+            if (firstOperatorValue == 0)
+            {
+                firstOperatorValue = currentNumberValue
+            }
+            currentNumberValue = Double(ResultLabel.text!)!
+        case "𝝅":
+            ResultLabel.text! = String(Double.pi)
+            if (firstOperatorValue == 0)
+            {
+                firstOperatorValue = currentNumberValue
+            }
+            currentNumberValue = Double(ResultLabel.text!)!
+        case "x^2":
+            if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
+            {
                 currentNumberValue = Double(ResultLabel.text!)!
-            case "x^2":
-                if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
-                {
-                    currentNumberValue = Double(ResultLabel.text!)!
-                    finalResult = String(currentNumberValue * currentNumberValue)
-                    displayResult(outputVal: finalResult)
-                }
-            case "x^y":
-                if(firstOperatorValue != 0)
-                {
-                    firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
-                }
-                mathOperationPerformed = true
-                mathOperator = "^"
-            case "C":
+                finalResult = String(currentNumberValue * currentNumberValue)
+                displayResult(outputVal: finalResult)
+            }
+        case "x^y":
+            if(firstOperatorValue != 0)
+            {
+                firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
+            }
+            mathOperationPerformed = true
+            mathOperator = "^"
+        case "C":
+            ResultLabel.text! = "0"
+            firstOperatorValue = 0
+            currentNumberValue = 0
+            mathOperationPerformed = false
+        case "E":
+            _ = ResultLabel.text!.popLast()
+            if(ResultLabel.text!.count < 1)
+            {
                 ResultLabel.text! = "0"
-                firstOperatorValue = 0
-                currentNumberValue = 0
-                mathOperationPerformed = false
-            case "E":
-                _ = ResultLabel.text!.popLast()
-                if(ResultLabel.text!.count < 1)
-                {
-                    ResultLabel.text! = "0"
-                }
-                else
-                {
-                    currentNumberValue = Double(ResultLabel.text!)!
-                }
-            case ".":
-                if(!ResultLabel.text!.contains("."))
-                {
-                    ResultLabel.text! += "."
-                }
-            case "+/-":
-                if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
-                {
-                    if(Int(ResultLabel.text!)! > 0)
-                    {
-                        ResultLabel.text! = "-" + ResultLabel.text!
-                    }
-                    else
-                    {
-                        ResultLabel.text! = String(abs(Int(ResultLabel.text!)!))
-                    }
-                    currentNumberValue = Double(ResultLabel.text!)!
-                }
-            case "+":
-                if(firstOperatorValue != 0)
-                {
-                    firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
-                }
-                mathOperationPerformed = true
-                mathOperator = "+"
-            case "-":
-                if(firstOperatorValue != 0)
-                {
-                    firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
-                }
-                mathOperationPerformed = true
-                mathOperator = "-"
-            case "/":
-                if(firstOperatorValue != 0)
-                {
-                    firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
-                }
-                mathOperationPerformed = true
-                mathOperator = "/"
-            case "x":
-                if(firstOperatorValue != 0)
-                {
-                    firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
-                }
-                mathOperationPerformed = true
-                mathOperator = "x"
-            case "%":
-                ResultLabel.text! = String(currentNumberValue/100)
+            }
+            else
+            {
                 currentNumberValue = Double(ResultLabel.text!)!
-            case "=":
-                if((ResultLabel.text! != "0") && (ResultLabel.text! != "") && (ResultLabel.text! != finalResult))
+            }
+        case ".":
+            if(!ResultLabel.text!.contains("."))
+            {
+                ResultLabel.text! += "."
+            }
+        case "+/-":
+            if((ResultLabel.text! != "0") && (ResultLabel.text! != ""))
+            {
+                if(Int(ResultLabel.text!)! > 0)
                 {
-                    finalResult = String(calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator))
-                    displayResult(outputVal: finalResult)
-                    finalResult = ResultLabel.text!
-                    firstOperatorValue = 0
-                }
-            default:
-                if (ResultLabel.text == "0")
-                {
-                    ResultLabel.text! = sender.titleLabel!.text!
-                    if (currentNumberValue == 0)
-                    {
-                        currentNumberValue = Double(ResultLabel.text!)!
-                    }
+                    ResultLabel.text! = "-" + ResultLabel.text!
                 }
                 else
                 {
-                    if (mathOperationPerformed == false)
-                    {
-                        ResultLabel.text! += sender.titleLabel!.text!
-                        currentNumberValue = Double(ResultLabel.text!)!
-                    }
-                    else
-                    {
-                        if (firstOperatorValue == 0)
-                        {
-                            firstOperatorValue = currentNumberValue
-                        }
-                        ResultLabel.text! = sender.titleLabel!.text!
-                        currentNumberValue = Double(ResultLabel.text!)!
-                        mathOperationPerformed = false
-                    }
+                    ResultLabel.text! = String(abs(Int(ResultLabel.text!)!))
                 }
-                print(currentNumberValue)
-                print(mathOperator)
+                currentNumberValue = Double(ResultLabel.text!)!
+            }
+        case "+":
+            if(firstOperatorValue != 0)
+            {
+                firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
+            }
+            mathOperationPerformed = true
+            mathOperator = "+"
+        case "-":
+            if(firstOperatorValue != 0)
+            {
+                firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
+            }
+            mathOperationPerformed = true
+            mathOperator = "-"
+        case "/":
+            if(firstOperatorValue != 0)
+            {
+                firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
+            }
+            mathOperationPerformed = true
+            mathOperator = "/"
+        case "x":
+            if(firstOperatorValue != 0)
+            {
+                firstOperatorValue = calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator)
+            }
+            mathOperationPerformed = true
+            mathOperator = "x"
+        case "%":
+            ResultLabel.text! = String(currentNumberValue/100)
+            currentNumberValue = Double(ResultLabel.text!)!
+        case "=":
+            if((ResultLabel.text! != "0") && (ResultLabel.text! != "") && (ResultLabel.text! != finalResult))
+            {
+                finalResult = String(calculateResult(firstVal: firstOperatorValue, secondVal: currentNumberValue, operandType: mathOperator))
+                displayResult(outputVal: finalResult)
+                finalResult = ResultLabel.text!
+                firstOperatorValue = 0
+            }
+        default:
+            if (ResultLabel.text == "0")
+            {
+                ResultLabel.text! = sender.titleLabel!.text!
+                if (currentNumberValue == 0)
+                {
+                    currentNumberValue = Double(ResultLabel.text!)!
+                }
+            }
+            else
+            {
+                if (mathOperationPerformed == false)
+                {
+                    ResultLabel.text! += sender.titleLabel!.text!
+                    currentNumberValue = Double(ResultLabel.text!)!
+                }
+                else
+                {
+                    if (firstOperatorValue == 0)
+                    {
+                        firstOperatorValue = currentNumberValue
+                    }
+                    ResultLabel.text! = sender.titleLabel!.text!
+                    currentNumberValue = Double(ResultLabel.text!)!
+                    mathOperationPerformed = false
+                }
+            }
+            print(currentNumberValue)
+            print(mathOperator)
         }
     }
     
