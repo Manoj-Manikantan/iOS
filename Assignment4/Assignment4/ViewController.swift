632b4fda2315c32fcca0fc82498773fcdca8c3a5
Manoj <manojmkt127@gmail.com>
2020-11-12 20:42:52 -0500

    Assignment 4 : Inital commit

---
+++ Assignment4/Assignment4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment4
+//
+//  Created by Manoj on 2020-11-12.
+//  Copyright © 2020 Manoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
