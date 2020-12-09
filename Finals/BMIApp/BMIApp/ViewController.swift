a4c8ef5d9fa60f883ae7f17d60d989234fb27424
Manoj <manojmkt127@gmail.com>
2020-12-08 20:11:01 -0500

    Final assignment initial commit

---
+++ Finals/BMIApp/BMIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by Manoj on 2020-12-08.
+//  Copyright © 2020 Manoj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
